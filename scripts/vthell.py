e95637d62e61fa194f85615dea3d713055bf4eab
NoAiOne <aimanmh@my.smccd.edu>
2020-05-01 11:53:45 +0700

    vthell: rerecord if streamlink hits failed to reload playlist. Video will be merged later, there might be missing scene because of this. But this will allow for more than 6 hours recording.
    Oh also start using path.join

--- scripts/vthell.py
+++ scripts/vthell.py
@@ -1,10 +1,12 @@
 import glob
 import json
 import logging
-import os
 import subprocess as sp
 import sys
 from datetime import datetime
+from os import getenv, remove
+from os.path import getsize, isfile
+from os.path import join as pjoin
 
 from discord_webhook import DiscordEmbed, DiscordWebhook
 
@@ -18,7 +20,7 @@ RCLONE_TARGET_BASE = (
 BASE_VENV_BIN = (
     "/media/sdac/mizore/pip3/bin/"  # path to created python3 virtualenv bin
 )
-DISCORD_WEBHOOK_URL = os.getenv("VTHELL_DISCORD_WEBHOOK", "")
+DISCORD_WEBHOOK_URL = getenv("VTHELL_DISCORD_WEBHOOK", "")
 
 """
 Cookies must be Netscape format or the one that can be opened with CURL.
@@ -32,7 +34,9 @@ COOKIES_NAME = "cookies.txt"  # Your cookies file name
 logging.basicConfig(
     level=logging.DEBUG,
     handlers=[
-        logging.FileHandler(BASE_VTHELL_PATH + "nvthell.log", "a", "utf-8")
+        logging.FileHandler(
+            pjoin(BASE_VTHELL_PATH, "nvthell.log"), "a", "utf-8"
+        )
     ],
     format="%(asctime)s %(name)-1s -- [%(levelname)s]: %(message)s",
     datefmt="%Y-%m-%d %H:%M:%S",
@@ -58,11 +62,11 @@ def announce_shit(msg="Unknown"):
 
 def find_and_parse_cookies() -> list:
     """Find provided cookies and parse them"""
-    if not os.path.isfile(BASE_VTHELL_PATH + COOKIES_NAME):
+    if not isfile(pjoin(BASE_VTHELL_PATH, COOKIES_NAME)):
         return []
     vtlog.info("Opening cookies file...")
     cookies_data = open(
-        BASE_VTHELL_PATH + COOKIES_NAME, "r", encoding="utf-8"
+        pjoin(BASE_VTHELL_PATH, COOKIES_NAME), "r", encoding="utf-8"
     ).readlines()
     vtlog.info("Parsing cookies file...")
     cookies_data = [c.rstrip() for c in cookies_data if not c.startswith("#")]
@@ -124,10 +128,12 @@ def find_res(line: str):
 
 reset_handler(False)
 
-with open(BASE_VTHELL_PATH + "/dataset/_youtube_mapping.json") as fp:
+dataset_path = pjoin(BASE_VTHELL_PATH, "dataset")
+
+with open(pjoin(dataset_path, "_youtube_mapping.json")) as fp:
     upload_mapping = json.load(fp)
 
-with open(BASE_VTHELL_PATH + "/dataset/_bilibili_mapping.json") as fp:
+with open(pjoin(dataset_path, "_bilibili_mapping.json")) as fp:
     bilibili_temp = json.load(fp)
     for k, v in bilibili_temp.items():
         upload_mapping[k] = v
@@ -145,7 +151,7 @@ console.setFormatter(formatter1)
 vtlog.addHandler(console)
 
 vtlog.info("Collecting jobs...")
-vthell_jobs = glob.glob(BASE_VTHELL_PATH + "jobs/*.json")
+vthell_jobs = glob.glob(pjoin(BASE_VTHELL_PATH, "jobs", "*.json"))
 
 if not vthell_jobs:
     vtlog.info("No jobs, exiting...")
@@ -153,9 +159,60 @@ if not vthell_jobs:
     print_end()
     exit(0)
 
+
+def run_streamlink_proc(STL_CMD, vt, reload_mode=False):
+    vtlog.info("Executing streamlink command!")
+    vtlog.debug(" ".join(STL_CMD))
+    override_err = False
+    req_limit_err = False
+    args_unk_err = False
+    discord_announced = False
+    reload_playlist = False
+
+    process = sp.Popen(
+        " ".join(STL_CMD),
+        stdout=sp.PIPE,
+        shell=True,
+        stderr=sp.STDOUT,
+        bufsize=1,
+    )
+    for line in iter(process.stdout.readline, b""):
+        line = line.decode("utf-8")
+        vtlog.info(line)
+        line = line.lower()
+        if "failed to reload playlist" in line:
+            vtlog.warn("Failed, reloading playlist.")
+            vtlog.warn("Will continue the job and merge together the result.")
+            reload_playlist = False
+            break
+        if line.startswith("error") or line.startswith("streamlink: error"):
+            if "read timeout" in line:
+                override_err = True
+            if "429 client error" in line:
+                req_limit_err = True
+            if "unrecognized" in line:
+                args_unk_err = True
+        if "opening stream" in line and not discord_announced:
+            discord_announced = True
+            if "resolution" in vt:
+                vtlog.debug("Finding resolution from output line...")
+                vt["resolution"] = find_res(line)
+                vtlog.debug("Resolution: {}".format(vt["resolution"]))
+            if not reload_mode:
+                announce_shit("Job " + vt["id"] + " started recording!")
+            else:
+                announce_shit("Job " + vt["id"] + " re-recording stream!")
+
+    process.stdout.close()
+    process.wait()
+    rc = process.returncode
+
+    return rc, override_err, req_limit_err, args_unk_err, reload_playlist, vt
+
+
 vthell_stream = None
+final_output = []
 for vthjs in vthell_jobs:
-    STREAMLINK_CMD = [BASE_VENV_BIN + "streamlink", "-o"]
     dtnow = datetime.now().timestamp()
     with open(vthjs, "r") as fp:
         vt = json.load(fp)
@@ -193,15 +250,10 @@ for vthjs in vthell_jobs:
     if vt["firstRun"]:
         vt["firstRun"] = False
     vtlog.info("Unrecorded stream found, starting...")
-    save_ts_name = (
-        "'" + BASE_VTHELL_PATH + "streamdump/" + vt["filename"] + ".ts'"
-    )
+    save_ts_name = pjoin(BASE_VTHELL_PATH, "streamdump", vt["filename"])
 
     vtlog.info("Starting job for: {}".format(vt["id"]))
-    vtlog.debug("Output: {}".format(save_ts_name))
-    STREAMLINK_CMD.append(save_ts_name)
-    STREAMLINK_CMD.extend(find_and_parse_cookies())
-    STREAMLINK_CMD.extend([vt["streamUrl"], "best"])
+    vtlog.debug("Output: {}".format(save_ts_name + ".ts"))
     vt["isDownloading"] = True
 
     with open(vthjs, "w") as fp:
@@ -213,42 +265,39 @@ for vthjs in vthell_jobs:
     )
     console.setFormatter(formatter2)
     vtlog.addHandler(console)
+    reload_mode = False
+    while True:
+        STREAMLINK_CMD = [pjoin(BASE_VENV_BIN, "streamlink"), "-o"]
+        if not reload_mode:
+            STREAMLINK_CMD.append("'" + save_ts_name + ".ts'")
+        else:
+            c_next = len(final_output)
+            STREAMLINK_CMD.append(
+                "'" + save_ts_name + "_{}.ts'".format(c_next)
+            )
+        final_output.append(STREAMLINK_CMD[2])
+        STREAMLINK_CMD.extend(find_and_parse_cookies())
+        STREAMLINK_CMD.extend([vt["streamUrl"], "best"])
 
-    vtlog.info("Executing streamlink command!")
-    vtlog.debug(" ".join(STREAMLINK_CMD))
-    override_err = False
-    req_limit_err = False
-    args_unk_err = False
-    discord_announced = False
+        override_err = False
+        req_limit_err = False
+        args_unk_err = False
+        discord_announced = False
+        reload_playlist = False
 
-    process = sp.Popen(
-        " ".join(STREAMLINK_CMD),
-        stdout=sp.PIPE,
-        shell=True,
-        stderr=sp.STDOUT,
-        bufsize=1,
-    )
-    for line in iter(process.stdout.readline, b""):
-        line = line.decode("utf-8")
-        vtlog.info(line)
-        line = line.lower()
-        if line.startswith("error") or line.startswith("streamlink: error"):
-            if "read timeout" in line:
-                override_err = True
-            if "429 client error" in line:
-                req_limit_err = True
-            if "unrecognized" in line:
-                args_unk_err = True
-        if "opening stream" in line and not discord_announced:
-            discord_announced = True
-            vtlog.debug("Finding resolution from output line...")
-            vt["resolution"] = find_res(line)
-            vtlog.debug("Resolution: {}".format(vt["resolution"]))
-            announce_shit("Job " + vt["id"] + " started recording!")
+        (
+            rc,
+            override_err,
+            req_limit_err,
+            args_unk_err,
+            reload_playlist,
+            vt,
+        ) = run_streamlink_proc(STREAMLINK_CMD, vt, reload_mode)
 
-    process.stdout.close()
-    process.wait()
-    rc = process.returncode
+        if not reload_playlist:
+            break
+        else:
+            reload_mode = True
 
     vtlog.info("Return code: {}".format(rc))
     vtlog.debug("Error overriden? {}".format(override_err))
@@ -265,27 +314,45 @@ for vthjs in vthell_jobs:
         vt["isDownloading"] = False
         with open(vthjs, "w") as fp:
             json.dump(vt, fp, indent=2)
+        final_output.clear()
         continue
     else:
         vthell_stream = vt
         break
 
-if not vthell_stream:
+if not vthell_stream or not final_output:
     vtlog.info("No scheduled streams that met conditions, exiting...")
     reset_handler()
     print_end()
     exit(0)
 
-vtjsf = BASE_VTHELL_PATH + "jobs/" + vthell_stream["id"] + ".json"
+vtjsf = pjoin(BASE_VTHELL_PATH, "jobs", vthell_stream["id"] + ".json")
 
-save_mux_name = BASE_VTHELL_PATH + "streamdump/" + vthell_stream["filename"]
+save_mux_name = pjoin(
+    BASE_VTHELL_PATH, "streamdump", vthell_stream["filename"]
+)
 if "resolution" in vthell_stream:
     if vthell_stream["resolution"]:
         save_mux_name += " [{} AAC]".format(vthell_stream["resolution"])
 save_mux_name += ".mkv"
-save_ts_name1 = (
-    BASE_VTHELL_PATH + "streamdump/" + vthell_stream["filename"] + ".ts"
-)
+final_output = [ts[1:-1] for ts in final_output]
+EXTRA_MKVMERGE = ["--track-order", "0:0,0:1"]
+if len(final_output) > 1:
+    copy_of_out = final_output[:]
+    EXTRA_MKVMERGE.append("--append-to")
+    APPEND_TO = []
+    for n, _ in enumerate(copy_of_out[1:], 1):
+        APPEND_TO.append("{a}:0:{b}:0".format(a=n, b=n - 1))
+    for n, _ in enumerate(copy_of_out[1:], 1):
+        APPEND_TO.append("{a}:1:{b}:1".format(a=n, b=n - 1))
+    EXTRA_MKVMERGE.append(",".join(APPEND_TO))
+
+save_ts_name1 = []
+for ts in final_output:
+    save_ts_name1.append(ts)
+    save_ts_name1.append("+")
+
+save_ts_name1 = save_ts_name1[:-1]
 
 announce_shit(
     "Stream ID: "
@@ -300,16 +367,16 @@ vthell_stream["isDownloaded"] = True
 with open(vtjsf, "w") as fp:
     json.dump(vthell_stream, fp)
 
-
 MKVMERGE_CMD.append(save_mux_name)
 MKVMERGE_CMD.extend(MKVMERGE_LANG)
-MKVMERGE_CMD.append(save_ts_name1)
+MKVMERGE_CMD.extend(save_ts_name1)
+MKVMERGE_CMD.extend(EXTRA_MKVMERGE)
 
 vtlog.info("Executing mkvmerge command!")
 vtlog.debug(" ".join(MKVMERGE_CMD))
 sp.call(MKVMERGE_CMD)
 
-if os.path.isfile(save_mux_name) and os.path.getsize(save_mux_name) > 0:
+if isfile(save_mux_name) and getsize(save_mux_name) > 0:
     UPLOAD_CMD.extend(
         [
             save_mux_name,
@@ -323,11 +390,12 @@ if os.path.isfile(save_mux_name) and os.path.getsize(save_mux_name) > 0:
     vtlog.info("Executing rclone command!")
     vtlog.debug(" ".join(UPLOAD_CMD))
     sp.call(UPLOAD_CMD)
-    os.remove(save_mux_name)
+    remove(save_mux_name)
 
 vtlog.info("Cleaning up...")
-os.remove(vtjsf)
-os.remove(save_ts_name1)
+remove(vtjsf)
+for ts in final_output:
+    remove(ts)
 
 reset_handler()
 print_end()
