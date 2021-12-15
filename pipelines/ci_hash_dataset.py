6f0723ae5df5628cd3b27fe11d64ee8b6991d19b
noaione <noaione0809@gmail.com>
2021-12-16 03:26:39 +0800

    Generate archive file

--- pipelines/ci_hash_dataset.py
+++ pipelines/ci_hash_dataset.py
@@ -2,10 +2,11 @@ import json
 from hashlib import md5
 from os.path import splitext
 from pathlib import Path
+from zipfile import ZipFile
 
 dataset_path = Path(__file__).absolute().parent.parent / "dataset"
 
-all_json_files = dataset_path.glob("**/*.json")
+all_json_files = list(dataset_path.glob("**/*.json"))
 print("[*] Loading datasets")
 compiled_dataset = {}
 for json_f in all_json_files:
@@ -22,3 +23,11 @@ hashed_data = md5(as_string.encode("utf-8")).hexdigest()
 print(f"[*] Current hash: {hashed_data}, saving to file...")
 with open(dataset_path / "currentversion", "w") as fp:
     fp.write(hashed_data)
+
+
+zip_archive = dataset_path / "archive.zip"
+print(f"[*] Creating archive {zip_archive}")
+with ZipFile(str(zip_archive), "w") as zip:
+    for json_f in all_json_files:
+        zip.write(str(json_f), json_f.name)
+print("[*] Done")
