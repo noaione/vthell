f2e67bc3e6e8173d3f21f9d29ed7902b248dbd36
noaione <noaione0809@gmail.com>
2021-12-27 13:13:40 +0800

    tasks(tempchat): add cancelled mark

--- internals/tasks/tempchat.py
+++ internals/tasks/tempchat.py
@@ -93,12 +93,15 @@ class TemporaryChatTasks(InternalTaskBase):
             VTHellJobStatus.preparing,
             VTHellJobStatus.downloading,
             VTHellJobStatus.error,
+            VTHellJobStatus.cancelled,
         ]:
             if chat_job.is_done:
                 logger.info(f"{task_name}: Job <{chat_job.id}> is done, dispatching upload...")
                 await app.dispatch("internals.chat.uploader", context={"job": chat_job, "app": app})
             else:
-                logger.warning(f"{task_name}: Job <{chat_job.id}> is finished, dispatching with force rewrite...")
+                logger.warning(
+                    f"{task_name}: Job <{chat_job.id}> is finished, dispatching with force rewrite..."
+                )
                 await app.dispatch(
                     "internals.chat.manager", context={"app": app, "video": video_data, "force": True}
                 )
