09eade15002e96150b4185dee7f3431b7596214b
noaione <noaione0809@gmail.com>
2022-01-11 10:01:19 +0800

    extractor: add missing annotations

--- internals/extractor/models.py
+++ internals/extractor/models.py
@@ -22,6 +22,8 @@ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.
 """
 
+from __future__ import annotations
+
 import asyncio
 from dataclasses import dataclass, field
 from typing import TYPE_CHECKING, Any, Callable, Coroutine, Dict, List, Optional
